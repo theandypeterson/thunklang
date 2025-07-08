d78ffd3fb89189cdd9ce8a3dc8142a525d4a7dbb
Andy Peterson <andy.peterson@atomicobject.com>
2025-07-08 07:46:25 -0400

    strings

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,13 @@
 package ast
 
-import "interpreter/token"
+import (
+	"bytes"
+	"interpreter/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -26,6 +30,15 @@ func (p *Program) TokenLiteral() string {
 	}
 	return ""
 }
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
 
 type Identifier struct {
 	Token token.Token
@@ -34,6 +47,7 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string       { return i.Value }
 
 type AssignmentStatement struct {
 	Name  *Identifier
@@ -42,3 +56,30 @@ type AssignmentStatement struct {
 
 func (ass *AssignmentStatement) statementNode()       {}
 func (ass *AssignmentStatement) TokenLiteral() string { return "" }
+func (ass *AssignmentStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ass.Name.String())
+	out.WriteString(" = ")
+
+	if ass.Value != nil {
+		out.WriteString(ass.Value.String())
+	}
+
+	return out.String()
+}
+
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+
+	return ""
+}
